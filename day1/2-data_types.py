2effa03f8e5847ab2d1c297d683bc40580791dc6
Charlie <cj@ponti.io>
2020-02-06 22:00:28 -0500

    (day1): add data_types lesson

---
+++ day1/2-data_types.py
@@ -0,0 +1,31 @@
+"""
+Lessons for learning data types in Python
+"""
+
+
+# Lists
+banks = ["charles schwab", "capital one", "american express"]
+
+# Dict
+banks = [
+    {"name": "charles schwab"},
+    {"name": "capital one"},
+    {"name": "american express"},
+]
+
+# Tuples
+cash_accounts = ("checking", "savings")
+investment_accounts = ("stock", "bond", "mutual_fund")
+credit_accounts = ("credit_card", "student_loan", "mortgage")
+print(max(credit_accounts))
+
+
+#------------------------------------------------------------------
+
+# List iteration
+for bank in banks:
+  print(bank)
+
+# Dict deconstruction
+bank_names = [bank["name"] for bank in banks]
+print(bank_names)
